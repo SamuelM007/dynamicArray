064933de13c7386d089b3116a569c30850b5a9ae
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-04-23 11:55:23 -0700

    test for get

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -5,5 +5,20 @@ import org.junit.jupiter.api.Test;
 public class DynamicStringListTest {
 
 
+    @Test
+    public void TestGetNormalValue(){
+        DynamicStringList list = new DynamicStringList();
+
+        for(int i = 0; i<10;i++){
+            list.add("Test"+i);
+        }
+
+        String actual = list.get(5);
+
+        assertEquals("Test5",actual);
+
+
+    }
+
     
 }
